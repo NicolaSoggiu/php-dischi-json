57fc1501b4d4e58e3da8b31df12911a8ef907487
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-09 15:02:51 +0200

    move data in script.php

--- script.js
+++ script.js
@@ -3,45 +3,17 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      arrCards: [
-        {
-          img: "",
-          title: "New Jersey",
-          author: "Bon Jovi",
-          year: "1988",
-        },
-        {
-          img: "",
-          title: "Live at Wembley 86",
-          author: "Queen",
-          year: "1992",
-        },
-        {
-          img: "",
-          title: "Ten's Summoner's Tales",
-          author: "Sting",
-          year: "1993",
-        },
-        {
-          img: "",
-          title: "Steve Gadd band",
-          author: "Steve Gadd Band",
-          year: "2018",
-        },
-        {
-          img: "",
-          title: "Brave new World",
-          author: "Iron Maiden",
-          year: "2000",
-        },
-        {
-          img: "",
-          title: "One more car, one more rider",
-          author: "Eric Clapton",
-          year: "2002",
-        },
-      ],
+      arrCards: [],
     };
   },
-  methods: {},
+  methods: {
+    requestCard() {
+      axios
+        .get("http://localhost/php-dischi-json/script.php")
+        .then((response) => (this.arrCards = response.data));
+    },
+  },
+  created() {
+    this.requestCard();
+  },
 }).mount("#app");
