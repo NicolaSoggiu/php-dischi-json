718f5ac4cf37b6478a9e52dfab0e3234083abad1
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-06-09 14:46:38 +0200

    add style and data in the script.js

--- script.js
+++ script.js
@@ -2,7 +2,46 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      arrCards: [
+        {
+          img: "",
+          title: "New Jersey",
+          author: "Bon Jovi",
+          year: "1988",
+        },
+        {
+          img: "",
+          title: "Live at Wembley 86",
+          author: "Queen",
+          year: "1992",
+        },
+        {
+          img: "",
+          title: "Ten's Summoner's Tales",
+          author: "Sting",
+          year: "1993",
+        },
+        {
+          img: "",
+          title: "Steve Gadd band",
+          author: "Steve Gadd Band",
+          year: "2018",
+        },
+        {
+          img: "",
+          title: "Brave new World",
+          author: "Iron Maiden",
+          year: "2000",
+        },
+        {
+          img: "",
+          title: "One more car, one more rider",
+          author: "Eric Clapton",
+          year: "2002",
+        },
+      ],
+    };
   },
   methods: {},
 }).mount("#app");
